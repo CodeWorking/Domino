ae275dcad4c61ac5bab6268948440f5dd108e740
Gustavo Andrés Angulo <woakas@gmail.com>
2012-07-09 09:37:11 -0500

    Adición de la ficha

---
+++ src/domino.py
@@ -0,0 +1,15 @@
+
+
+class Ficha():
+    """
+    """
+
+    def __init__(self, a, b):
+        """
+        Crear una Ficha
+        Arguments:
+        - `a`: El primer lado de la ficha
+        - `b`: El segundo lado de la ficha
+        """
+        self.lado1 = a
+        self.lado2 = b
