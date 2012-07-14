686e210ff52d173273b133626318ceb9b08e17f8
Diego <diego@diego-HP-Pavilion-dv6000-KM715LA-ABM.(none)>
2012-07-14 16:15:58 -0500

    Creacion de la clase Jugador

--- src/domino.py
+++ src/domino.py
@@ -23,3 +23,27 @@ class Ficha():
 class Tablero():
     def __unicode__(self):
         return "Tablero"
+
+
+
+
+class Jugador():
+    """
+    """
+
+    def __init__(self, n,t):
+        """
+        Crear un Jugador
+        Arguments:
+        - `nombre`: El nombre del jugador
+        - `fichas`: Numero de fichas
+        - `tipo`  : Tipo de
+
+        """
+        self.nombre = n
+        self.fichas = []
+        self.tipo = t
+
+    def __unicode__(self):
+        return "(%s, %s, %s)" % (self.nombre, self.fichas, self.tipo)
+
