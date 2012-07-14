f59b5116e0fff1deab61787fee54858ae6745e8c
Fabian Castro <inghfcastrol@gmail.com>
2012-07-14 16:33:46 -0500

    Definición de la clase domino

--- src/domino.py
+++ src/domino.py
@@ -1,5 +1,4 @@
 
-
 class Ficha():
     """
     """
@@ -23,3 +22,29 @@ class Ficha():
 class Tablero():
     def __unicode__(self):
         return "Tablero"
+
+class Domino():
+    def __init__(self, nummayor, numjugadores):
+        """
+        nummayor: el numero mayor de las fichas
+        numjugadores: numero de participantes del juego
+        fichasrobar: numero de fichas que sobran
+        """
+        self.nummayor = 0
+        self.jugadores = [Jugador()]
+        self.fichasrobar = []
+        self.turno = 0
+        self.tablero = Tablero()
+        self.crearFichas()
+        self.crearJugadores()
+        self.repartirFichas()
+
+    def crearFichas():
+        for x in range(self.nummayor):
+            for j in range(x, self.nummayor):
+                self.fichasrobar.append(Ficha(x,j))         
+        
+    
+    def __unicode__(self):
+        return "(%s, %s)" & (self.nummayor, self.numjugadores)
+
